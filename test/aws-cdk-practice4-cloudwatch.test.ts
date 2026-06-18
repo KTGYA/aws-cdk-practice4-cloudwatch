9e0e06dbabafb1e265a3b99e902d7a1dd29a4b3c
KTGYA <284801140+KTGYA@users.noreply.github.com>
2026-06-18 15:21:28 +0900

    Initial commit

---
+++ test/aws-cdk-practice4-cloudwatch.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsCdkPractice4Cloudwatch from '../lib/aws-cdk-practice4-cloudwatch-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-cdk-practice4-cloudwatch-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsCdkPractice4Cloudwatch.AwsCdkPractice4CloudwatchStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
